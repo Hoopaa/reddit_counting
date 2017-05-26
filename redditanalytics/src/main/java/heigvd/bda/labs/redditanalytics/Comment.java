1a22e04744968d31cef5f3791b31c5469b0f9e77
gerome12 <gerome.pasquier@gmail.com>
2017-05-26 13:11:11 +0200

    fichier de lecture de JSON

---
+++ redditanalytics/src/main/java/heigvd/bda/labs/redditanalytics/Comment.java
@@ -0,0 +1,58 @@
+package test;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Comment {
+	private JSONObject json;
+	
+	Comment(String json)
+	{
+		try {
+			this.json = new JSONObject(json);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public String getBody()
+	{
+		try {
+			return (String) json.get("body");
+		} catch (JSONException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	public String getLinkId()
+	{
+		try {
+			return (String) json.get("link_id");
+		} catch (JSONException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	public String getId()
+	{
+		try {
+			return (String) json.get("id");
+		} catch (JSONException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	public String getParentId()
+	{
+		try {
+			return (String) json.get("parent_id");
+		} catch (JSONException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+}
---
+++ redditanalytics/src/main/java/heigvd/bda/labs/redditanalytics/Submission.java
@@ -0,0 +1,58 @@
+package test;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Submission {
+	private JSONObject json;
+	
+	Submission(String json)
+	{
+		try {
+			this.json = new JSONObject(json);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public String getId()
+	{
+		try {
+			return (String) json.get("id");
+		} catch (JSONException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	public String getTitle()
+	{
+		try {
+			return (String) json.get("title");
+		} catch (JSONException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	public String getSubredditId()
+	{
+		try {
+			return (String) json.get("subreddit_id");
+		} catch (JSONException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	public String getScore()
+	{
+		try {
+			return (String) json.get("score");
+		} catch (JSONException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+}
